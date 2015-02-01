9ccc4b86189c0f34e29ae7ec9f207bcdbde8669a
77web <info@77-web.com>
2015-02-01 14:54:39 +0900

    [App] 処理の流れをコメントアウトして記述

--- src/App.php
+++ src/App.php
@@ -8,4 +8,27 @@ namespace Nagoya\Vol8;
 
 class App
 {
+    private $inputParser;
+
+    private $mapBuilder;
+
+    /**
+     * @param string $input
+     * @return int
+     */
+    public function run($input)
+    {
+        // 点の定義を二次元配列に分割する
+        // $points = $this->inputParser->parse($input);
+
+        // 点の定義をPointのマップに変換する
+        // $map = $this->mapBuilder->build($points);
+
+
+        // ルーターに調べさせる
+        //$router = new Router($map);
+        //$maxHop = $router->getMaxHop();
+
+        //return $maxHop;
+    }
 }
--- tests/AppTest.php
+++ tests/AppTest.php
@@ -4,5 +4,20 @@ namespace Nagoya\Vol8;
 
 class AppTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @param string|null $input
+     * @param int|null $expected
+     */
+    public function test($input = null, $expected = null)
+    {
+        $app = new App();
+        $actual = $app->run($input);
 
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function provideTestData()
+    {
+        return [];
+    }
 }
